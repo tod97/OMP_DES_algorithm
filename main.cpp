365c4d1a9a9b291940809674b20b4cb2b7a55555
Francesco <todinofrancesco97@gmail.com>
2023-12-26 15:10:25 +0100

    test init omp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <omp.h> // for OpenMP library functions
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -25,6 +26,27 @@ void sequentialDES(vector<string> lines, int size)
 	}
 }
 
+void parallelDES(vector<string> lines, int size, int nThreads)
+{
+#pragma omp parallel
+	{
+		DESAlgorithm *des = new DESAlgorithm();
+		omp_set_num_threads(nThreads);
+
+#pragma omp for
+		for (int j = 0; j < size; j++)
+		{
+			string text = lines[j];
+
+			string encryptedText = des->DES(des->stringToBin(text));
+			des->reverseKeys();
+			string decryptedText = des->DES(encryptedText);
+
+			string result = des->binToString(decryptedText);
+		}
+	}
+}
+
 int main()
 {
 	ifstream file("text_gen/words.txt");
@@ -38,4 +60,11 @@ int main()
 	auto end = system_clock::now();
 	auto elapsed = duration_cast<milliseconds>(end - start);
 	cout << "seq DES (n=" << partialWords << "): " << elapsed.count() << "ms" << endl;
+
+	// PARALLEL
+	start = system_clock::now();
+	parallelDES(lines, partialWords, 4);
+	end = system_clock::now();
+	elapsed = duration_cast<milliseconds>(end - start);
+	cout << "par DES (n=" << partialWords << "): " << elapsed.count() << "ms" << endl;
 }
