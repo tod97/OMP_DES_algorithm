f5d625d368027f6dc059d23739b77ca6e09e8e35
Francesco <todinofrancesco97@gmail.com>
2023-12-26 12:03:40 +0100

    added words generator

---
+++ text_gen/words_gen.py
@@ -0,0 +1,23 @@
+import random
+import string
+import os
+
+def words_gen(N, length):
+    characters = string.ascii_letters + string.digits + './'
+
+    words = []
+    for _ in range(N):
+        new_word = ''.join(random.choice(characters) for _ in range(length))
+        words.append(new_word)
+
+    return words
+
+words = words_gen(10000, 8)
+
+filename = os.path.join(os.path.dirname(__file__), 'words.txt')
+
+with open(filename, 'w') as file:
+    if words:
+        file.write(words[0])
+        for word in words[1:]:
+            file.write('\n' + word)
